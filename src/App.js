7d4ba2d287108977056bbe9aa68e76857cf8e1f5
Thongzxje <thonghoangdanh@gmail.com>
2022-09-30 15:08:29 +0300

    Start styling up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './index.css';
 
 import CountryForm from './CountryForm';
 
-import NavBar from './NavBar';
+
+import { Form, Navbar, Container} from 'react-bootstrap';
 
 const App = () => {
   const [countrys, setCountrys] = useState([]);
@@ -28,17 +29,28 @@ const App = () => {
     const value = event.target.value.toLowerCase();
     const filteredList = countrys.filter(country => country.name.toLowerCase().includes(value));
     setFiltered(filteredList);
-  }
+  }  
+
 
   return (
     <>
-    <NavBar />
-    <div>
-      <input type='text' placeholder='Search' onChange={handleSearch} />
-    </div>
-    <div>
-        {filtered.map(country =><CountryForm country={country}/>)}
-    </div>
+    <Navbar bg="dark" variant='dark'>
+        <Container>
+          <Navbar.Brand>Country Dictionary</Navbar.Brand>
+        </Container>
+        <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+              onChange={handleSearch}
+            />
+        </Form>
+    </Navbar>
+  
+    {filtered.map(country =><CountryForm country={country}/>)}
+  
     </>
     
   )
--- src/CountryForm.js
+++ src/CountryForm.js
@@ -1,12 +1,38 @@
 import {useState} from 'react';
 import Weather from './Weather';
+import { Card, Button } from 'react-bootstrap';
 
 const CountryForm = ({country}) => {
     const [isShowed, setShowed] = useState(false);
+
+    const Default = ({country}) => {
+      return (
+      <div class='country-card'>
+        <Card>
+      <Card.Img variant="top" src={country.flags.svg} style={{width: 150}}/>
+      <Card.Body>
+        <Card.Title>{country.name}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary" onClick={() => setShowed(!isShowed)}>View</Button>
+      </Card.Body>
+      <Card.Footer>
+          <small className="text-muted">Click view button for more detail</small>
+        </Card.Footer>
+      </Card>
+      </div>
+      )
+    }
+
     const Detail = ({country}) => {
       return (
-        <div>
-            <img src={country.flags.svg} alt="not found" />
+      <div>
+        <Card>
+      <Card.Img variant="top" src={country.flags.svg} style={{width: 150}} />
+      <Card.Body>
+        <Card.Title>{country.name}</Card.Title>
             <p><b>Region</b> {country.region}</p>
             <p><b>Capital</b> {country.capital}</p>
             <p><b>Area</b> {country.area}</p>
@@ -15,17 +41,20 @@ const CountryForm = ({country}) => {
                 {country.languages.map(language => <li>{language.name}</li>)}
             </ul>
             <Weather capital={country.capital}/>
-            <button onClick={() => setShowed(!isShowed)}>Collapse</button>
-        </div>
+            <Button variant='primary' onClick={() => setShowed(!isShowed)}>Collapse</Button>
+      </Card.Body>
+      <Card.Footer>
+          <small className="text-muted">Click view button for more detail</small>
+        </Card.Footer>
+      </Card>
+      </div>
+      
       )
   }
     return (
-      <div>
-        <h4>{country.name}</h4>
-        <div>
-          {!isShowed ? <button onClick={() => setShowed(!isShowed)}>Detail</button> : <Detail country={country} />}
-        </div>
-      </div>
+      <>
+      {!isShowed ? <Default country={country} /> : <Detail country={country} />}
+      </>
     )
   }
 
